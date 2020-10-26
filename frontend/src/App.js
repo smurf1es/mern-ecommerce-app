c115913b53c6d01de69cc4f5ffee3c82cf2a8ac0
telkomdev-pahuger <hughdev101@gmail.com>
2020-10-26 21:05:48 +0700

    Backend products.js added, already set to fetch data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
-  <React.Fragment>
-    <h1>ProShop</h1>
-  </React.Fragment>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Route exact path="/" component={HomeScreen} />
+          <Route path="/product/:id" component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </BrowserRouter>
+  );
 };
 
 export default App;
